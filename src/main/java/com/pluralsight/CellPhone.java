c57b361b3fb18273519c4a0e06d84d328a564681
RickelviC <Cruzrickelvi@gmail.com>
2026-04-14 14:43:11 -0400

    add cellphone setter and getter

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,58 @@
 package com.pluralsight;
 
 public class CellPhone {
+
+    private int serialNumber = 0;
+    private String model = "";
+    private String carrier = "";
+    private String phoneNumber = "";
+    private String owner = "";
+
+    public CellPhone(int serialNumber, String owner, String phoneNumber, String carrier, String model) {
+        this.serialNumber = serialNumber;
+        this.owner = owner;
+        this.phoneNumber = phoneNumber;
+        this.carrier = carrier;
+        this.model = model;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
